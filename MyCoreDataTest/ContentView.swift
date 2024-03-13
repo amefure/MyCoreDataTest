1f231a48979e56f77dab15c4f3ef912b53bb5a60
amefure <shachi10969@gmail.com>
2024-03-13 23:09:13 +0900

    完成

--- MyCoreDataTest/ContentView.swift
+++ MyCoreDataTest/ContentView.swift
@@ -10,105 +10,66 @@ import CoreData
 
 struct ContentView: View {
     
-    @State var persons: Array<Person> = []
-    
-    @State var companys: Array<Company> = []
-    
-    @State var selectTab = 0
+    @State private var companys: Array<Company> = []
     
     var body: some View {
         
-        TabView(selection: $selectTab) {
-            List {
-                Button {
-//                    DispatchQueue.global(qos: .background).async {
-                        addPerson()
-//                    }
-                } label: {
-                    Label("Add Item Person", systemImage: "plus")
+        List {
+            Button {
+                DispatchQueue.global(qos: .background).async {
+                    addCompany()
                 }
-                
-                ForEach(companys, id: \.self) { item in
-                    HStack {
-                        if let name = item.id?.uuidString {
-                            Text(name)
-                        } else if item.name == "" {
-                            Text("NOne")
-                        } else {
-                            Text("nil")
-                        }
+            } label: {
+                Label("Add Item Person", systemImage: "plus")
+            }
+            
+            ForEach(companys, id: \.self) { item in
+                HStack {
+                    if let name = item.name {
+                        Text(name)
+                    } else if item.name == "" {
+                        Text("NOne")
+                    } else {
+                        Text("nil")
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }.tag(0)
-            
-            
-            List {
-                Button {
-//                    DispatchQueue.global(qos: .background).async {
-                        addCompany()
-//                    }
-                    
-                } label: {
-                    Label("Add Item", systemImage: "plus")
-                }
-                ForEach(companys, id: \.self) { item in
-                    HStack {
-                        Text(item.name ?? "")
-                        Text("\(item.location ?? "")")
-                        Text("\(item.person!.count)")
-                    }
-                }
-            }.tag(1)
+            }
+            .onDelete(perform: deleteItems)
             
         }.onAppear {
-//            DispatchQueue.global(qos: .background).async {
-                CoreDataRepository2.shared.fetch(onBackgroundThread: true) { (result: [Person]) in
-                    persons = result
-                }
-                CoreDataRepository2.shared.fetch(onBackgroundThread: true) { (result: [Company]) in
+            DispatchQueue.global(qos: .background).async {
+                CoreDataRepository.shared.fetch { (result: [Company]) in
                     print(companys)
                     companys = result
                 }
-//            }
+            }
         }
     }
     
-    private func addPerson() {
-        CoreDataRepository2.shared.newEntity(onBackgroundThread: true) { (newCompany: Company) in
+    private func addCompany() {
+        CoreDataRepository.shared.newEntity(onBackgroundThread: false) { (newCompany: Company) in
             // 新しいエンティティにデータを設定
             newCompany.id = UUID()
             newCompany.name = "Web制作会社"
             newCompany.location = "東京都"
             
             // 新しいエンティティを保存
-            CoreDataRepository2.shared.insert(newCompany, onBackgroundThread: true)
+            CoreDataRepository.shared.insert(newCompany, onBackgroundThread: false)
             
-            CoreDataRepository2.shared.fetch(onBackgroundThread: true) { (result: [Company]) in
+            CoreDataRepository.shared.fetch { (result: [Company]) in
                 companys = result
             }
         }
+    }
+    
+    
+    func deleteItems(at offsets: IndexSet) {
+        for index in offsets {
+            let companyToDelete = companys[index]
+            CoreDataRepository.shared.delete(companyToDelete, onBackgroundThread: false)
+        }
+    }
 
-            
-        
-    }
-    
-    private func addCompany() {
-//        withAnimation {
-//            let newCompany: Company = CoreDataRepository2.shared.newEntity()
-//            newCompany.id = UUID()
-//            newCompany.name = "ABCデザイン"
-//            newCompany.location = "東京都"
-//            
-//            CoreDataRepository2.shared.insert(newCompany)
-//            
-//            companys = CoreDataRepository2.shared.fetch()
-//        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        
-    }
 }
 
 #Preview {
--- MyCoreDataTest/CoreDataRepository.swift
+++ MyCoreDataTest/CoreDataRepository.swift
@@ -6,171 +6,15 @@
 //
 
 import CoreData
-//
-//class CoreDataRepository {
-//    
-//    /// ファイル名
-//    private static let persistentName = "MyCoreDataTest"
-//    
-//    ///
-//    private static var persistenceController: NSPersistentContainer = {
-//        let container = NSPersistentContainer(name: CoreDataRepository.persistentName)
-//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-//            if let error = error as NSError? {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        })
-//        container.viewContext.automaticallyMergesChangesFromParent = true
-//        return container
-//    }()
-//    
-//    private static var context: NSManagedObjectContext {
-//        return CoreDataRepository.persistenceController.viewContext
-//    }
-//    
-//    private static var contextBG: NSManagedObjectContext {
-//        return CoreDataRepository.persistenceController.newBackgroundContext()
-//    }
-//}
-//
-//
-//extension CoreDataRepository {
-//    
-//    /// 新規作成
-//    static func entity<T: NSManagedObject>() -> T {
-//        let entityDescription = NSEntityDescription.entity(forEntityName: String(describing: T.self), in: context)!
-//        return T(entity: entityDescription, insertInto: nil)
-//    }
-//    
-//    /// 取得処理
-//    static func fetch<T: NSManagedObject>() -> [T] {
-//        let fetchRequest = NSFetchRequest<T>(entityName: String(describing: T.self))
-//
-//        do {
-//            return try context.fetch(fetchRequest)
-//        } catch let error as NSError {
-//          print("Could not fetch. \(error), \(error.userInfo)")
-//            return []
-//        }
-//    }
-//    
-//    /// 取得処理
-////    static func fetch<T: NSManagedObject>(completion: @escaping ([T]) -> Void) {
-////        contextBG.perform {
-////            let fetchRequest = NSFetchRequest<T>(entityName: String(describing: T.self))
-////            do {
-////                let result =  try contextBG.fetch(fetchRequest)
-////                completion(result)
-////            } catch let error as NSError {
-////                print("Could not fetch. \(error), \(error.userInfo)")
-////                completion([])
-////            }
-////        }
-////        
-////    }
-//    
-//    
-//    /// 新規作成
-//    static func newEntity<T: NSManagedObject>() -> T {
-//        let entity = NSEntityDescription.insertNewObject(forEntityName: String(describing: T.self), into: context)
-//        return entity as! T
-//    }
-//    
-//    /// 新規作成
-//    static func newLocator() -> Person {
-//        let entity = NSEntityDescription.insertNewObject(forEntityName: String(describing: Person.self), into: context)
-//        return entity as! Person
-//    }
-//
-//    
-//    /// 追加処理
-//    static func insert(_ object: NSManagedObject) {
-//        context.insert(object)
-//    }
-//    
-//    /// 削除処理
-//    static func delete(_ object: NSManagedObject) {
-//        context.delete(object)
-//    }
-//    /// 保存処理
-//    static func save() {
-//        // 変更がある場合のみ
-//        guard context.hasChanges else { return }
-//        do {
-//            try context.save()
-//        } catch let error as NSError {
-//            fatalError("Unresolved error \(error), \(error.userInfo)")
-//        }
-//    }
-//}
 
-
-//class CoreDataRepository2 {
-//    static let shared = CoreDataRepository2()
-//    
-//    
-//    private var persistenceController: NSPersistentContainer = {
-//        let container = NSPersistentContainer(name: "MyCoreDataTest")
-//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-//            if let error = error as NSError? {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        })
-//        container.viewContext.automaticallyMergesChangesFromParent = true
-//        return container
-//    }()
-//    
-//    
-//    /// 新規作成
-//    func newEntity<T: NSManagedObject>() -> T {
-//        var context = persistenceController.viewContext
-//        let entity = NSEntityDescription.insertNewObject(forEntityName: String(describing: T.self), into: context)
-//        return entity as! T
-//    }
-//    
-//    /// 新規作成
-//    func newLocator() -> Person {
-//        var context = persistenceController.viewContext
-//        let entity = NSEntityDescription.insertNewObject(forEntityName: String(describing: Person.self), into: context)
-//        return entity as! Person
-//    }
-//    
-//    
-//    /// 追加処理
-//    func insert(_ object: NSManagedObject) {
-//        var context = persistenceController.viewContext
-//        context.insert(object)
-//        guard context.hasChanges else { return }
-//        do {
-//            try context.save()
-//        } catch let error as NSError {
-//            fatalError("Unresolved error \(error), \(error.userInfo)")
-//        }
-//    }
-//    
-//
-//    /// 取得処理
-//    func fetch<T: NSManagedObject>() -> [T] {
-//        var context = persistenceController.viewContext
-//        let fetchRequest = NSFetchRequest<T>(entityName: String(describing: T.self))
-//
-//        do {
-//            return try context.fetch(fetchRequest)
-//        } catch let error as NSError {
-//          print("Could not fetch. \(error), \(error.userInfo)")
-//            return []
-//        }
-//    }
-//}
-
-
-
-
-class CoreDataRepository2 {
-    static let shared = CoreDataRepository2()
+class CoreDataRepository {
+    
+    static let shared = CoreDataRepository()
+    
+    private static let persistentName = "MyCoreDataTest"
     
     private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "MyCoreDataTest")
+        let container = NSPersistentContainer(name: CoreDataRepository.persistentName)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
@@ -187,20 +31,26 @@ class CoreDataRepository2 {
     }
     
     /// 新規作成
-    func newEntity<T: NSManagedObject>(onBackgroundThread: Bool = false, completion: @escaping (T) -> Void) {
+    public func newEntity<T: NSManagedObject>(onBackgroundThread: Bool = false, completion: @escaping (T) -> Void) {
         let context = onBackgroundThread ? makeBackgroundContext() : persistentContainer.viewContext
         context.perform {
             let entity = NSEntityDescription.entity(forEntityName: String(describing: T.self), in: context)!
-            let newObject = T(entity: entity, insertInto: context)
+            let newObject = T(entity: entity, insertInto: nil)
             completion(newObject)
         }
     }
     
     /// 追加処理
-    func insert(_ object: NSManagedObject, onBackgroundThread: Bool = false) {
-        print((object as? Company)?.id)
-        print((object as? Company)?.name)
+    public func insert(_ object: NSManagedObject, onBackgroundThread: Bool = false) {
         let context = onBackgroundThread ? object.managedObjectContext ?? makeBackgroundContext() : persistentContainer.viewContext
+        context.insert(object)
+        saveContext(context)
+    }
+    
+    /// 削除処理
+    public func delete(_ object: NSManagedObject, onBackgroundThread: Bool = false) {
+        let context = onBackgroundThread ? object.managedObjectContext ?? makeBackgroundContext() : persistentContainer.viewContext
+        context.delete(object)
         saveContext(context)
     }
     
@@ -215,8 +65,8 @@ class CoreDataRepository2 {
     }
 
     /// 取得処理
-    func fetch<T: NSManagedObject>(onBackgroundThread: Bool = false, completion: @escaping ([T]) -> Void) {
-        let context = onBackgroundThread ? makeBackgroundContext() : persistentContainer.viewContext
+    public func fetch<T: NSManagedObject>(completion: @escaping ([T]) -> Void) {
+        let context = persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<T>(entityName: String(describing: T.self))
 
         context.perform {
